3444e4728c7761367cf2941e30920fa14fb6787c
DarkJoe <gray78181878@gmail.com>
2022-07-27 12:49:34 +0800

    3rd commit

--- Program.cs
+++ Program.cs
@@ -15,3 +15,69 @@
 // // Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
 // // Console.Write($"{Environment.NewLine}Press any key to exit...");
 // // Console.ReadKey(true);
+
+//Output
+// you can add as many console as you like
+
+// Console.WriteLine("Hello World!");
+// Console.WriteLine("I am Learning C#");
+// Console.WriteLine("It is awesome!");
+// int
+/* How you comment -- The code below will print the words 
+Hello World to the screen, and it is amazing */
+// Console.WriteLine(3 + 3);
+
+// Variables/types
+
+// int myNum = 5;
+// double myDoubleNum = 5.99D;
+// char myLetter = 'D';
+// bool myBool = true;
+// string myText = "Hello";
+// Console.WriteLine(myNum);
+// Console.WriteLine(myDoubleNum);
+// Console.WriteLine(myLetter);
+// Console.WriteLine(myBool);
+// Console.WriteLine(myText);
+
+// string firstName = "Joezer ";
+// string lastName = "Cardoza";
+// string fullName = firstName + lastName;
+// Console.WriteLine(fullName);
+
+// int x = 5;
+// int y = 6;
+// Console.WriteLine(x + y);
+
+// float is 6 or 7 digits while double is 15 digits long
+
+// float f1 = 35e3F;
+// double d1 = 12E4D;
+// Console.WriteLine(f1);
+// Console.WriteLine(d1);
+
+// converstion of int to string
+
+// int myInt = 10;
+// Console.WriteLine(Convert.ToString(myInt));
+
+// Types of Converstion 
+
+// int myInt = 9;
+// double myDouble = myInt;       // Automatic casting: int to double
+
+// Console.WriteLine(myInt);      // Outputs 9
+// Console.WriteLine(myDouble);   // Outputs 9
+
+// Console.WriteLine("Enter your age:");
+// int age = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Your age is: " + age);
+
+// Console.Write("Enter your name: ");
+
+// string myName = Console.ReadLine();
+
+// Console.WriteLine("Your name is "+ myName);
+
+// string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Console.WriteLine("The length of the txt string is: " + txt.Length);
